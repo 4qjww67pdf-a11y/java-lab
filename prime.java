4f10613f023d56c0c9954649e99131c1da745aef
Aayush sah <scammery51@gmail.com>
2026-05-15 12:11:56 +0545

    initial comment

--- prime.java
+++ prime.java
@@ -2,27 +2,37 @@ import java.util.Scanner;
 
 public class prime {
     public static void main(String[] args) {
-        int num, i, count = 0;
+        Scanner input = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter a number to check if it is prime 0 to exit:");
+            int num = input.nextInt();
 
-        System.out.print("Enter a number: ");
-        num = sc.nextInt();
+            if (num == 0) {
+                System.out.println("We are exiting the program.");
+                break;
+            }
 
-        if (num <= 1) {
-            System.out.println(num + " is not a Prime Number");
-        } else {
-            for (i = 1; i <= num; i++) {
-                if (num % i == 0) {
-                    count++;
+            boolean isPrime = true;
+
+            if (num <= 1) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(num); i++) {
+                    if (num % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
                 }
             }
 
-            if (count == 2) {
-                System.out.println(num + " is a Prime Number");
+            if (isPrime) {
+                System.out.printf("The number %d is prime\n", num);
             } else {
-                System.out.println(num + " is not a Prime Number");
+                System.out.printf("The number %d is not prime\n", num);
             }
         }
+
+        input.close();
     }
 }
