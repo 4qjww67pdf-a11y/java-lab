4e719aa9e98894f5d3d82fe35c3b3103faaa54a1
Aayush sah <scammery51@gmail.com>
2026-05-15 11:26:25 +0545

    initial comment

---
+++ factorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class factorial {
+   public static void main(String[] args){   
+   Scanner input=new Scanner(System.in);
+   while(true){
+   System.out.println("\nEnter a number for factorial or 0 to exit:");
+   int num=input.nextInt();
+   if(num==0){
+    System.out.println("We are exiting the program:");
+    break;
+   }
+   else{
+   long fact=1;
+   for(int i=1;i<=num;i++){
+    fact=fact*i;
+   }
+   System.out.printf("The factorial of %d is %d",num,fact);
+}
+}
+   input.close();
+   }
+}
